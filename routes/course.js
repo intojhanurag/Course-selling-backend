7edd63c3a4d7ab974b2a2313f8583210da4ee04a
Anurag Ojha <aojharaj2004@gmail.com>
2025-01-15 19:31:03 +0530

    Added purchases

--- routes/course.js
+++ routes/course.js
@@ -1,13 +1,26 @@
 const {Router}=require("express");
+const { userMiddleware } = require("../middleware/user");
+const {purchaseModel, courseModel}=require("../db")
 const courseRouter=Router();
-courseRouter.post("/purchase",function(req,res){
+courseRouter.post("/purchase",userMiddleware,async function(req,res){
+    //you would expect the user to pay your money
+    const userId=req.userId;
+    const courseId=req.body.courseId;
+//should check that user has actually paid the price
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
     res.json({
-        message:"signup endpoint"
+        message:"You have successfully bought the course"
     })
 })
-courseRouter.get("/preview",function(req,res){
+
+
+courseRouter.get("/preview",async function(req,res){
+    const courses=await courseModel.find({});
     res.json({
-        message:"signup endpoint"
+        courses
     })
 })
 module.exports={
--- routes/user.js
+++ routes/user.js
@@ -49,9 +49,14 @@ userRouter.post("/signin",async function(req,res){
     }
     
 })
-userRouter.get("/purchases",function(req,res){
+userRouter.get("/purchases",userMiddleware,async function(req,res){
+    const userId=req.userId;
+
+    const purchases=await purchaseModel.find({
+        userId
+    })
     res.json({
-        message:"signup endpoint"
+        purchases
     })
 })
 
